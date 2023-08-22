c0cd10663550e712e4ae931dfe46a7670d90afff
Larissa Manso <mansolarissa19@gmail.com>
2023-08-22 11:27:44 -0300

    update

---
+++ 2208/exercicio6.js
@@ -0,0 +1,9 @@
+let inicial = 200;
+let ganhou = 150;
+let perdeu = 250;
+let final = inicial + ganhou - perdeu;
+
+console.log("Produtos iniciais", inicial);
+console.log("Produtos adicionais", ganhou);
+console.log("Produtos vendidos", perdeu);
+console.log("Produtos restantes", final);
