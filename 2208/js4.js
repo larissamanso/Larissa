4ae7cbdc665f740f6592e7ea02800aec96e8ec81
Larissa Manso <mansolarissa19@gmail.com>
2023-08-22 09:11:39 -0300

    update

---
+++ 2208/js4.js
@@ -0,0 +1,16 @@
+let resultado= 10 / 0;
+console.log(resultado); //Resultado: Infinity
+
+let multiplicacao = 5 * 3;
+console.log(multiplicacao1); //Resultado: 15
+
+let decimal1 = 2.5;
+let decimal2 = 1.5;
+multiplicacao = decimal1 * decimal2;
+console.log(multiplicacao2); //Resultado: 3.75
+
+resultado = 10 * 0;
+console.log(resultado3); //Resultado: 0
+
+resultado = 2 + 3 * 4;
+console.log(resultado4); //Resultado: 14
