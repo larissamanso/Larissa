03ccf2bb49d288cb5251e1e2309a30d9c464b06e
Larissa Manso <mansolarissa19@gmail.com>
2023-08-22 11:14:24 -0300

    upgrade

---
+++ 2208/exercicio4.js
@@ -0,0 +1,9 @@
+let salario = 9.857;
+let aluguel = 2.545;
+let contas = 1.987;
+let extras = 2.522;
+ let saldo = salario - (aluguel + contas + extras);
+ 
+ console.log("Seu salário é:", salario);
+ console.log("Você paga", aluguel, "de aluguel,", contas, "de contas e tem", extras, "de gastos extras");
+ console.log("Seu saldo disponível é:", saldo);
