40ef565bb23bc5f84feebb88f86ebd7b95ef93b4
Larissa Manso <mansolarissa19@gmail.com>
2023-08-22 09:36:17 -0300

    update

---
+++ 2208/js7.js
@@ -0,0 +1,7 @@
+let texto = "Olá, mundo!";
+let novoTexto = texto.replace("mundo", "JavaScript");
+console.log(novoTexto); //Resultado: "Olá, JavaScript!"
+
+texto = "Olá, mundo!";
+console.log(texto.includes("mundo")); //Resultado: true
+console.log(texto.includes("JavaScript")); //Resultado: false
