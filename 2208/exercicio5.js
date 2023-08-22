217017ae9f601252920f1077d3eb16e349667daa
Larissa Manso <mansolarissa19@gmail.com>
2023-08-22 11:21:43 -0300

    update

--- 2208/exercicio5.js
+++ 2208/exercicio5.js
@@ -1,4 +1,9 @@
 let inicial = 50;
 let ganhou = 30;
 let perdeu = 15;
-let valor = inicial + ganhou 
+let valor = inicial + ganhou - perdeu;
+
+console.log("Pontuação inicial", inicial);
+console.log("Pontos extras", ganhou);
+console.log("Pontos perdidos", perdeu);
+console.log("Pontuação final", valor);
