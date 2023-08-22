1c9942c387eb462812e87f2ef7fbcd3fdc1dea9b
Larissa Manso <mansolarissa19@gmail.com>
2023-08-22 11:18:24 -0300

    update

--- 2208/exercicio4.js
+++ 2208/exercicio4.js
@@ -6,4 +6,4 @@ let extras = 2.522;
  
  console.log("Seu salário é:", salario);
  console.log("Você paga", aluguel, "de aluguel,", contas, "de contas e tem", extras, "de gastos extras");
- console.log("Seu saldo disponível é:", saldo);
+ console.log("Seu saldo disponível é:", saldo.toFixed(3));
---
+++ 2208/exercicio5.js
@@ -0,0 +1,4 @@
+let inicial = 50;
+let ganhou = 30;
+let perdeu = 15;
+let valor = inicial + ganhou 
