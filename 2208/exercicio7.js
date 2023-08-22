3f57c028c33c17db1639aa6b8bd7bc7a2ce4fdb7
Larissa Manso <mansolarissa19@gmail.com>
2023-08-22 11:32:10 -0300

    update

---
+++ 2208/exercicio7.js
@@ -0,0 +1,9 @@
+let pai = 53;
+let mae = 50;
+let filha = 17;
+let soma = pai + mae + filha;
+
+console.log("Idade do pai", pai);
+console.log("Idade da mãe", mae);
+console.log("Idade da filha", filha);
+console.log("Soma das idades:", soma);
