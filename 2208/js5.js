290b905c580c53a3aae2fc3f64cb2bbd62c68537
Larissa Manso <mansolarissa19@gmail.com>
2023-08-22 09:18:58 -0300

    update

---
+++ 2208/js5.js
@@ -0,0 +1,18 @@
+let resultado1 = (2+3) * 4;
+console.log(resultado1); //Resultado: 20
+
+let resultado2 = 10 + 5 - 2;
+console.log(resultado2); //Resultado: 13
+
+let resultado3 = (10 + 5) * 2 - 3;
+console.log(resultado3); //Resultado: 27
+
+let num1 = 3;
+let num2 = 4;
+let resultado = num1 * (num1 + num2);
+console.log(resultado4); //Resultado: 21
+
+const PI = 3.14;
+let raio = 5;
+let area = PI * raio * raio;
+console.log(area); //Resultado: 78.5
