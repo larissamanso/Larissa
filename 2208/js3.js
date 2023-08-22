667d394d3ce199f8fa584e9fd1f063094146b1f2
Larissa Manso <mansolarissa19@gmail.com>
2023-08-22 09:01:04 -0300

    update

--- 2208/js3.js
+++ 2208/js3.js
@@ -5,3 +5,16 @@ let decimal1 = 5.7;
 let decimal2 = 2.3;
 subtracao = decimal1 - decimal2;
 console.log(subtracao2); //Resultado: 3.4
+
+let num1 = 10;
+let num2 = 3;
+let resultado = num1 - num2;
+console.log(resultado); //Resultado: 7
+
+let divisao = 10 / 2;
+console.log(divisao); //Resultado: 5
+
+decimal1 = 5.5;
+decimal2 = 2;
+divisao = decimal1 / decimal2;
+console.log(divisao2); //Resultado: 2.75
