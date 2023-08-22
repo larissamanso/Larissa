326696f5b50b77cabef20d3464031b138cffbd47
Larissa Manso <mansolarissa19@gmail.com>
2023-08-22 08:41:20 -0300

    update

--- 2208/js2.js
+++ 2208/js2.js
@@ -3,3 +3,8 @@ let num2 = 4;
 let num3 = 6;
 let soma1 = num1 + num2 + num3;
 console.log(soma1); //Resultado: 12
+
+let decimal1 = 2.5;
+let decimal2 = 1.3;
+let soma2 = decimal1 + decimal2;
+console.log(soma2); //Resultado: 3.8
