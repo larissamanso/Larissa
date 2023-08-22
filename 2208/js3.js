e7fec72e2a29e2d4ebb14573c8f20d150154f3c8
Larissa Manso <mansolarissa19@gmail.com>
2023-08-22 08:56:08 -0300

    update

--- 2208/js3.js
+++ 2208/js3.js
@@ -1,2 +1,7 @@
 let subtracao = 8 - 3;
 console.log(subtracao1); //Resultado: 5
+
+let decimal1 = 5.7;
+let decimal2 = 2.3;
+subtracao = decimal1 - decimal2;
+console.log(subtracao2); //Resultado: 3.4
